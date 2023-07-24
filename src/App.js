f31d15699089968eac2d412922e182a38bf28ce1
Alex Tsi <alex.tsi.dev@gmail.com>
2023-07-24 19:42:15 +0200

    add button and state and see how real DOM is changed via adding new html by React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
+import Button from "./components/UI/Button/Button";
 
 function App() {
+    const [isParagraphShown, setIsParagraphShown] = useState(false)
+    const onClickBtn = () => {
+        (setIsParagraphShown(
+            prevShow => !prevShow
+        ))
+    }
+
     return (
         <div className="app">
             <h1>Hi there!</h1>
+            {isParagraphShown && <p>New Paragraph</p>}
+            <Button onClick={onClickBtn}>Show Paragraph</Button>
         </div>
     );
 }
